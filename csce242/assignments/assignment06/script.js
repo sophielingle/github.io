f18755298cb5b0237bb9ecb2d874dcec0be558d3
Sophie Lingle <slingle@email.sc.edul@domain.com>
2024-09-22 15:41:19 -0400

    adding assignment 6

---
+++ csce242/assignments/assignment06/script.js
@@ -0,0 +1,60 @@
+/* Nav Bar Icon */
+document.querySelector("#toggle-nav-down").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hidden-small");
+    document.getElementById("toggle-nav-up").classList.toggle("hidden");
+    document.querySelector("#toggle-nav-down").classList.toggle("hidden");
+};
+
+document.querySelector("#toggle-nav-up").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hidden-small");
+    document.querySelector("#toggle-nav-up").classList.toggle("hidden");
+    document.getElementById("toggle-nav-down").classList.toggle("hidden");
+};
+
+document.getElementById("exercise1").onclick = (event) => {
+    event.preventDefault(); 
+    document.getElementById("exercise1-div").classList.add("show");
+    document.getElementById("exercise2-div").classList.remove("show");
+};
+
+document.getElementById("exercise2").onclick = (event) => {
+    event.preventDefault(); 
+    document.getElementById("exercise1-div").classList.remove("show");
+    document.getElementById("exercise2-div").classList.add("show");
+};
+
+/* Exercise 1 */
+document.getElementById("myRange").onchange = (e) => {
+    document.querySelector(":root").style.setProperty("--r-value", e.target.value);
+    const color = e.target.value;
+    const messageP = document.getElementById("color-message");
+
+    if (color >= 150) {
+        messageP.innerHTML = "bright red";
+    }
+    else if (color <= 80) {
+        messageP.innerHTML = "very dark red";
+    }
+    else {
+        messageP.innerHTML = "dark red";
+    }
+};
+
+/* Exercise 2 */
+document.querySelector("button.size-btn").onclick = (e) => {
+    if (e.target.innerHTML == "Small") {
+        document.getElementById("random-1").classList.toggle("hidden");
+        document.getElementById("random-2").classList.add("hidden");
+        document.getElementById("random-3").classList.add("hidden");
+    }
+    else if (e.target.innerHTML == "Medium") {
+        document.getElementById("random-1").classList.add("hidden");
+        document.getElementById("random-2").classList.toggle("hidden");
+        document.getElementById("random-3").classList.add("hidden");
+    }
+    else if (e.target.innerHTML == "Large") {
+        document.getElementById("random-1").classList.add("hidden");
+        document.getElementById("random-2").classList.add("hidden");
+        document.getElementById("random-3").classList.toggle("hidden");
+    }
+};
