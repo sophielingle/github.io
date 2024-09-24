fbfce65e3b3505e407dc7eec4173fc7a5cd3f491
Sophie Lingle <slingle@email.sc.edul@domain.com>
2024-09-24 17:16:47 -0400

    adding loops assignment

---
+++ csce242/assignments/assignment07/script.js
@@ -0,0 +1,35 @@
+//Draw Button
+document.getElementById("draw-btn").onclick = () => {
+    const map = document.getElementById("space");
+    const amount = parseInt(document.getElementById("star-number").value);
+    const error = document.getElementById("error");
+    error.innerHTML = "";
+
+    if(isNaN(amount) || amount <= 0) {
+        error.innerHTML = "*Invalid Input*";
+        return;
+    }
+
+    map.innerHTML = "";
+
+    const starSize = 20; 
+    const mapWidth = 400;
+    const mapHeight = 500;
+
+    for(let i = 0; i < amount; i++) {
+        const star = document.createElement("div");
+        star.setAttribute("class", "star");
+
+        const x = Math.random() * (mapWidth-starSize); 
+        const y = Math.random() * (mapHeight-starSize); 
+
+        star.style.left = `${x}px`;
+        star.style.top = `${y}px`;
+
+        map.append(star);
+
+        star.addEventListener('click', function() {
+            alert(`You clicked on star ${i+1}`);
+        });
+    }
+}
