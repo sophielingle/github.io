1ded36845c8d41da4b35c11c52fdbcaf2ca0bce1
Sophie Lingle <slingle@email.sc.edul@domain.com>
2024-09-26 19:16:31 -0400

    adding assignment 8

---
+++ csce242/assignments/assignment08/script.js
@@ -0,0 +1,39 @@
+let titles = [];
+titles["images/birthday.jpg"] = 'Birthdays';
+titles["images/clown.jpg"] = 'Clown';
+titles["images/rain.jpg"] = 'Rainy Day';
+titles["images/read.jpg"] = 'Books';
+titles["images/shovel.jpg"] = 'Gardening';
+titles["images/work.jpg"] = 'Working';
+
+let descriptions = [];
+descriptions["images/birthday.jpg"] = 'Happy Birthday!';
+descriptions["images/clown.jpg"] = 'Have a balloon!';
+descriptions["images/rain.jpg"] = 'You need an umbrella!';
+descriptions["images/read.jpg"] = 'Do you like to read?';
+descriptions["images/shovel.jpg"] = 'You might need to wear gloves!';
+descriptions["images/work.jpg"] = 'Get to work!';
+
+/* Associative array loop */
+const table = document.querySelector("#img-table");
+let count = 0;
+
+for(let image in titles) {
+    if (count % 3 === 0) {
+        var row = document.createElement("tr");
+        table.append(row);
+    }
+    const data = document.createElement("td");
+    const pic = document.createElement("img");
+    pic.src = image;
+    pic.classList.add('images'); 
+    data.append(pic);
+    row.append(data);
+
+    pic.addEventListener('click', () => {
+    document.getElementById('title').innerHTML = titles[image];
+    document.getElementById('description').innerHTML = descriptions[image];
+    });
+
+    count++;
+}
--- csce242/exercises/08_loops/script.js
+++ csce242/exercises/08_loops/script.js
@@ -6,17 +6,98 @@ document.querySelector("#toggle-nav").onclick = () => {
 };
 
 document.getElementById("btn-loop").onclick = () => {
+  const startNum = parseInt(document.getElementById("txt-first-num").value);
+  const endNum = parseInt(document.getElementById("txt-last-num").value);
   const ul = document.getElementById("numbers");
 
-  for(let i = 0; i < 10; i++){
-    const li = document.createElement("li");
-    li.innerHTML = i+1;
-    ul.append(li);
-    li.setAttribute("id", "li" + i);
+  console.log(`counting from ${startNum} to ${endNum}`);
 
-    li.onclick = () => {
-      console.log(`Lucky number ${i+1}`);
-    };
+  if(endNum >= startNum){
+    for(let i = startNum; i <= endNum; i++){
+      ul.append(createLi(i));
+    }
+  } else {
+    for(let i = endNum; i >= startNum; i--){
+      ul.append(createLi(i));
+    }
   }
 }
 
+const createLi = (num) => {
+  const li = document.createElement("li");
+  li.innerHTML = num;
+  li.setAttribute("id", "li" + num);
+
+  li.onclick = () => {
+    console.log(`Lucky number ${num}`);
+  };
+
+  return li;
+}
+
+let counter = 0;
+let updateCount;
+
+document.getElementById("btn-count").onclick = (e) => {
+  const count = document.getElementById("count");
+
+  if(e.target.innerHTML == "Start"){
+    e.target.innerHTML = "Stop";
+    updateCount = setInterval(()=>{
+      counter++;
+      count.innerHTML = counter;
+    },1000);
+
+  } else {
+    e.target.innerHTML = "Start";
+    clearInterval(updateCount);
+  }
+
+}
+
+/* Learning arrays */
+let toys = ["drum", "ball", "rope", "ballon", "tire"];
+
+/*
+for(let i = 0; i < toys.length; i++){
+  console.log(toys[i]);
+}*/
+
+/* Shows that you can get the index if you need it
+toys.forEach((toy, i)=> {
+  console.log(i + toy);
+});
+*/
+
+toys.forEach((toy)=> {
+  const p = document.createElement("p");
+  p.innerHTML = toy;
+  document.getElementById("list").append(p);
+});
+
+/* Santas expenses */
+let toyPrices = [];
+toyPrices["barbie"] = 7.54;
+toyPrices["doll house"] = 86.23;
+toyPrices["slide"] = 34.23;
+toyPrices["ken"] = 5.3;
+toyPrices["bike"] = 7.4;
+
+/* Associative array */
+const table = document.querySelector("#list-prices tbody");
+
+for(let toy in toyPrices){
+  let tr = document.createElement("tr");
+  table.append(tr);
+  const td1 = document.createElement("td");
+  td1.innerHTML = toy;
+  tr.append(td1);
+
+  const td2 = document.createElement("td");
+  td2.innerHTML = toyPrices[toy];
+  tr.append(td2);
+  
+  tr.onclick = () => {
+    console.log(toy + ": " + toyPrices[toy]);
+  }
+}
