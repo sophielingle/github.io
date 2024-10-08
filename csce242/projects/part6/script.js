ebfc06c8ac78b54db3df4d3b9cd27ce9205b4bd3
Sophie Lingle <slingle@email.sc.edul@domain.com>
2024-10-08 16:07:03 -0400

    editing part6

--- csce242/projects/part6/script.js
+++ csce242/projects/part6/script.js
@@ -2,3 +2,42 @@
 document.querySelector("#toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hidden-small");
 };
+
+/* Best Sellers Page Load */
+const getBooks = async () => {
+    const url = "https://portiaportia.github.io/json/ice-creams.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showIceCream = async () => {
+    const iceCreams = await getIceCream();
+
+    iceCreams.forEach((iceCream) => {
+        document.getElementById("ice-cream-section").append(getIceCreamSection(iceCream));
+    });
+};
+
+const getIceCreamSection = (iceCream) => {
+    const section = document.createElement("section");
+    section.classList.add("ice-cream");
+    section.classList.add("container");
+   
+    const img = document.createElement("img");
+    img.src = `https://portiaportia.github.io/json/images/ice-creams/${iceCream.image}`;
+    section.append(img);
+
+    const div = document.createElement("div");
+    div.classList.add("overlay");
+    div.innerHTML = iceCream.name;
+    section.append(div);
+
+    return section;
+};
+
+showIceCream();
